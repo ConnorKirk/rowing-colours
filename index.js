2eea99855b2436e65e56b7edce0fb030939f24e9
Connor Kirkpatrick <connor.kirkpatrick@hotmail.com>
2022-11-07 22:57:09 +0000

    Set answers

--- index.js
+++ index.js
@@ -3,16 +3,41 @@ const button1 = document.querySelector("button:nth-child(1)");
 const button2 = document.querySelector("button:nth-child(2)");
 const button3 = document.querySelector("button:nth-child(3)");
 
+const checkAnswer = (event) => {
+  event.preventDefault();
+
+  if (event.target.value === answer) {
+    console.log("victory!");
+  } else {
+    console.log("fail");
+  }
+};
+
+const buttons = document.querySelectorAll("button.choice");
+buttons.forEach((button) => {
+  button.addEventListener("click", checkAnswer);
+});
+
+let answer = "";
+
 const newRound = () => {
   const club = getRandomChoice(clubs);
+  const wrongChoice1 = getRandomChoice(clubs);
+  const wrongChoice2 = getRandomChoice(clubs);
 
+  answer = club.name;
   updateBlade(img, club);
 
   // set the buttons
+
+  button1.innerText = wrongChoice1.name;
+  button1.value = wrongChoice1.name;
+
   button2.innerText = club.name;
-  button1.innerText,
-    (button3.innerText = getRandomChoice(clubs).name),
-    getRandomChoice(clubs.name);
+  button2.value = club.name;
+
+  button3.innerText = wrongChoice2.name;
+  button3.value = wrongChoice2.name;
 };
 
 const getRandomChoice = (arr) => arr[Math.floor(Math.random() * arr.length)];
@@ -20,5 +45,7 @@ const updateBlade = (imgNode, club) => {
   imgNode.src = club.imageUrl;
   imgNode.alt = club.description;
 };
+
+
 
 newRound();
