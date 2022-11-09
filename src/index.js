6457b9321d76537a9abb0196959789078247a8e8
Connor Kirkpatrick <connor.kirkpatrick@hotmail.com>
2022-11-09 23:04:15 +0000

    Shuffle location of correct answer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ const button1 = document.querySelector("button:nth-child(1)");
 const button2 = document.querySelector("button:nth-child(2)");
 const button3 = document.querySelector("button:nth-child(3)");
 
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
 const checkAnswer = (event) => {
   event.preventDefault();
 
@@ -29,16 +37,19 @@ const newRound = () => {
   answer = club.name;
   updateBlade(img, club);
 
+  const arr = [club, wrongChoice1, wrongChoice2];
+  shuffle(arr);
+
   // set the buttons
 
-  button1.innerText = wrongChoice1.name;
-  button1.value = wrongChoice1.name;
+  button1.innerText = arr[0].name;
+  button1.value = arr[0].name;
 
-  button2.innerText = club.name;
-  button2.value = club.name;
+  button2.innerText = arr[1].name;
+  button2.value = arr[1].name;
 
-  button3.innerText = wrongChoice2.name;
-  button3.value = wrongChoice2.name;
+  button3.innerText = arr[2].name;
+  button3.value = arr[2].name;
 };
 
 const getRandomChoice = (arr) => arr[Math.floor(Math.random() * arr.length)];
